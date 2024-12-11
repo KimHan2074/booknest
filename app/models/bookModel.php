0da3986ab71aeeb2339331930f511c83f54ce6ad
Phạm Đức Đạt <91515103+Dominic2k@users.noreply.github.com>
2024-12-11 23:43:54 +0700

    write model, controller and sent the result of book best selling to homepage

--- app/models/bookModel.php
+++ app/models/bookModel.php
@@ -26,7 +26,25 @@ class bookModel extends DModel {
         return $this->db->select($sql, $data);
     }
 
+    public function getBestSellingBookHomepage($table_books) {
+        $sql = "
+        SELECT 
+            $table_books.title, 
+            $table_books.price, 
+            MIN(i.path) AS image_path
+        FROM 
+            $table_books
+        LEFT JOIN 
+            images i ON $table_books.book_id = i.book_id
+        GROUP BY 
+            $table_books.book_id, $table_books.title, $table_books.price
+        ORDER BY 
+            $table_books.stock ASC, $table_books.book_id ASC
+        LIMIT 10;
+        ";
 
+        return $this->db->select($sql);
+    }
 
 
 
