f5264ffe53856f0b2f2b5e2c6e64c7f4f2e9b037
Hasangnguyen <sanghanguyen05@gmail.com>
2024-12-11 17:21:26 +0700

    done title, price and quantity book

--- app/views/bookdetails.php
+++ app/views/bookdetails.php
@@ -62,6 +62,63 @@
         .small-book-image:hover {
             transform: scale(1.1);
         }
+
+        .title {
+            font-size: 24px;
+            font-weight: bold;
+            color: #000000;
+            padding-top: 30px;
+        }
+
+        .details-wrapper {
+            display: flex;
+            flex-direction: column;
+            gap: 50px;
+            flex: 1;
+        }
+        .price-quantity-wrapper {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+
+        .price {
+            font-size: 18px;
+            display: flex;
+            gap: 10px;
+        }
+
+        .original-price {
+            text-decoration: line-through;
+            color: #888;
+            font-weight: bold;
+        }
+
+        .discounted-price {
+            color: #F30B0B;
+            font-weight: bold;
+        }
+
+        .quantity-wrapper {
+            display: flex;
+            align-items: center;
+            gap: 10px;
+        }
+
+        .quantity-buttons {
+            display: flex;
+            padding-right: 80px ;
+            gap: 10px;
+        }
+
+        .quantity-input {
+            width: 30px;
+            text-align: center;
+            padding: 5px;
+            border: 1px solid  #F9F5EE;
+            border-radius: 3px;
+        }
+        
     </style>
 </head>
 <body>
@@ -78,6 +135,22 @@
                     <img src="https://product.hstatic.net/200000845405/product/3_694ddc374ca645378bcffe9d381c5849_master.jpg" alt="Small Image 3" class="small-book-image">
                 </div>
             </div>
+            <div class="details-wrapper">
+                <div class="title">Everyone is a wounded child behind an adult shell</div>
+                <div class="price-quantity-wrapper">
+                    <div class="price">
+                        <span class="original-price">30,000đ</span>
+                        <span class="discounted-price">27,000đ</span>
+                    </div>
+                    <div class="quantity-wrapper">
+                        <label for="quantity">Quantity:</label>
+                        <div class="quantity-buttons">
+                            <button class="btn-decrement">-</button>
+                            <button class="quantity-input" id="quantity">1</button>
+                            <button class="btn-increment">+</button>
+                        </div>
+                    </div>
+                </div>
 
 </body>
 </html>
