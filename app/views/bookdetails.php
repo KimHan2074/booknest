3a2782a5e78033f8206f6449daedae191de572f7
Hasangnguyen <sanghanguyen05@gmail.com>
2024-12-13 13:58:51 +0700

    update bookdetails

--- app/views/bookdetails.php
+++ app/views/bookdetails.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200&family=Inter:wght@400;600&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../../public/css/homepage.css">
-    <link rel="stylesheet" href="../../public/css/book_details.css">
+    <link rel="stylesheet" href="../public/css/homepage.css">
+    <link rel="stylesheet" href="../public/css/book_details.css">
     <title>BookDetails</title>
 </head>
 <body>
--- app/views/homepage.php
+++ app/views/homepage.php
@@ -17,7 +17,7 @@ $is_logged_in = isset($_SESSION['current']) && !empty($_SESSION['current']);
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200&family=Inter:wght@400;600&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Tinos:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../../public/css/homepage.css">
+    <link rel="stylesheet" href="public/css/homepage.css">
 </head>
 <body>
     <!-- Header -->
@@ -48,7 +48,7 @@ $is_logged_in = isset($_SESSION['current']) && !empty($_SESSION['current']);
                     <div class="product-item-bestSeller">
                         <img class="image-book" src="public/img/<?php echo $value['image_path']?>" alt="image-book">
                         <div class="name-price">
-                            <a class="name-book" href="http://localhost/booknest_website/bookController/showBookDetail?book_id=<?php echo $value['book_id'] ?>"><?php echo $value['title']?></a>
+                            <a class="name-book" href="/booknest_website/bookController/showBookDetail?book_id=<?php echo $value['book_id'] ?>"><?php echo $value['title']?></a>
                             <p class="price-book"><?php echo $value['price'] . "đ"?></p>
                         </div>
                     </div>
@@ -58,7 +58,7 @@ $is_logged_in = isset($_SESSION['current']) && !empty($_SESSION['current']);
                 </div>
             </div>
         </div>
-
+        
         <div class="content-right">
             <!-- Slider -->
             <div class="slider-container">
